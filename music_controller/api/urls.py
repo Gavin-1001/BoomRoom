b60f0c3f044e4510c6359e4f7d94f8c138c3e4b1
Gavin-1001 <gavinshelley10@gmail.com>
2022-07-09 18:47:20 +0100

    Updated requirements.txt

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import main
 
 urlpatterns = [
-    path('home', main) 
+    path('', main) 
     ##If this has a blank url, should send you to localhost:8000/ and it will call the function named main defined in api/views
 ]
