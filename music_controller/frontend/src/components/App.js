2d9661dd24d384090b81587823db9f87ba701cc7
Gavin-1001 <gavinshelley10@gmail.com>
2022-07-16 16:21:55 +0100

    Added a homepage component, added css styling

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React,{Component}from"react";
 import{render}from"react-dom";
+import homepage from "./homepage";
 
 
 export default class App extends Component{
---
+++ music_controller/frontend/src/components/homepage.js
@@ -0,0 +1,11 @@
+import React,{Component}from"react";
+
+
+export default class homepage extends Component{
+    constructor(props){
+      super(props); //parent constructor which is Component
+   }
+    render() {
+      return <homepage />;
+   }
+  }
